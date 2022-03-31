b4d21cc09a95cc10f20efedba11f5357a6811de6
sundaramawasthi <sawasthi0001@gmail.com>
2022-03-31 19:54:57 +0530

    update

---
+++ BASIC/PROGRAM-1.cpp
@@ -0,0 +1,20 @@
+//WAT TO PRINT NUMBER IS POSITIVE OR NOT 
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    int a ;
+    cout<<"ENTER:";
+    cin>>a;
+
+    if(a>0){
+        cout<<"POSITIVE";
+    }
+    else{
+        cout<<"NOT POSITIVE";
+    }
+}
---
+++ PROGRAM-8.cpp
@@ -0,0 +1,14 @@
+// PROGRAM TO USE POWER
+#include<math.h>
+#include<iostream>
+using namespace std ;
+
+int main(){
+    
+    int a ,b ;
+    cin>>a>>b;
+    
+    int power = pow(a,b);
+    cout<<"POWER is =  " << power;
+    
+}
