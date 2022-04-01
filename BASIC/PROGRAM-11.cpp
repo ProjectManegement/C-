c5de6881119003bee83f89f91e797abf2c8accc5
sundaramawasthi <sawasthi0001@gmail.com>
2022-04-01 12:42:53 +0530

    more question

---
+++ BASIC/PROGRAM-11.cpp
@@ -0,0 +1,19 @@
+/// PRINT 1234.. AS RECTANGLE 
+
+int main(){
+    
+    int n,i=1;
+    cout<<"ENTER:";
+    cin>>n;
+    
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<< j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+        
+    }
+}
